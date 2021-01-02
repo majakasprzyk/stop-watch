23939bc2bf6d959aaa57836d9e933875a6a08e16
Maja Kasprzyk <maja.kasprzyk@gmail.com>
2021-01-02 22:22:01 +0100

    time shows as a string

--- src/script.js
+++ src/script.js
@@ -7,3 +7,21 @@ const pauseBtn = document.querySelector(".pauseBtn");
 const saveBtn = document.querySelector(".saveBtn");
 const resetBtn = document.querySelector(".resetBtn");
 const timer = document.querySelector("timer");
+
+
+function timeToString(time) {
+    let hours = time / 3600000;
+    let hh = Math.floor(hours);
+  
+    let minutes = (diffInHrs - hh) * 60;
+    let mm = Math.floor(minutes);
+  
+    let seconds = (diffInMin - mm) * 60;
+    let ss = Math.floor(seconds);
+  
+    let formattedHH = hh.toString().padStart(2, "0");
+    let formattedMM = mm.toString().padStart(2, "0");
+    let formattedSS = ss.toString().padStart(2, "0");
+  
+    return `${formattedHH}:${formattedMM}:${formattedSS}`;
+  };
