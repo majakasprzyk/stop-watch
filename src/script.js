799a4d7c86684484f5a91e1e7c86a607115545f3
Maja Kasprzyk <maja.kasprzyk@gmail.com>
2021-01-02 20:40:46 +0100

    query selectors added

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
 console.log('Good luck!')
+
+const startBtn = document.querySelector(".startBtn")
+const nextBtn = document.querySelector(".nextBtn");
+const stopBtn = document.querySelector(".stopBtn");
+const pauseBtn = document.querySelector(".pauseBtn");
+const saveBtn = document.querySelector(".saveBtn");
+const resetBtn = document.querySelector(".resetBtn");
+const timer = document.querySelector("timer");
