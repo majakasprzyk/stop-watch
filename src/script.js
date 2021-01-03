0bb9af9cafb1af11f277ca4882b4f3a0f0f87343
Maja Kasprzyk <maja.kasprzyk@gmail.com>
2021-01-03 19:24:32 +0100

    classes added and startBtn

--- src/script.js
+++ src/script.js
@@ -1,22 +1,23 @@
 console.log('Good luck!')
 
+const timer = document.querySelector(".timer");
 const startBtn = document.querySelector(".startBtn")
 const nextBtn = document.querySelector(".nextBtn");
 const stopBtn = document.querySelector(".stopBtn");
 const pauseBtn = document.querySelector(".pauseBtn");
 const saveBtn = document.querySelector(".saveBtn");
 const resetBtn = document.querySelector(".resetBtn");
-const timer = document.querySelector("timer");
 
+// shows time as a string
 
 function timeToString(time) {
     let hours = time / 3600000;
     let hh = Math.floor(hours);
   
-    let minutes = (diffInHrs - hh) * 60;
+    let minutes = (hours - hh) * 60;
     let mm = Math.floor(minutes);
   
-    let seconds = (diffInMin - mm) * 60;
+    let seconds = (minutes - mm) * 60;
     let ss = Math.floor(seconds);
   
     let formattedHH = hh.toString().padStart(2, "0");
@@ -26,7 +27,24 @@ function timeToString(time) {
     return `${formattedHH}:${formattedMM}:${formattedSS}`;
   };
 
-  startBtn.addEventListener("click", start);
-  pauseBtn.addEventListener("click", pause);
-  resetBtn.addEventListener("click", reset);
+
+  let startTime;
+  let elapsedTime;
+
+
+  // start button 
   
+  startBtn.addEventListener('click', () => {
+    startTime = Date.now();
+    setInterval(function printTime(){
+        elapsedTime = Date.now() - startTime;
+        timer.innerHTML = timeToString(elapsedTime);
+    }, 1000);
+  });
+
+
+
+  // pauseBtn.addEventListener("click", pause);
+  // resetBtn.addEventListener("click", reset);
+  
+
